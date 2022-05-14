7a447b8921fc01f42a3fb471c926cf101008d627
DipHire <diphire111@gmail.com>
2022-05-14 18:26:31 +0530

    added files

---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:money/Failure_model.dart';
+import 'package:money/budget_repository.dart';
+import 'package:money/item_model.dart';
+import 'package:money/spending_chart.dart';
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Paisa app',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: BudgetScreen(),
+    );
+  }
+}
+
+class BudgetScreen extends StatefulWidget {
+  const BudgetScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BudgetScreen> createState() => _BudgetScreenState();
+}
+
+class _BudgetScreenState extends State<BudgetScreen> {
+  late Future<List<Item>> _futureItems;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Paisa Manager'),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          _futureItems = BudgetRepository().getItems();
+          setState(() {});
+        },
+        child: FutureBuilder<List<Item>>(
+          future: _futureItems,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              //Show pie chart
+              final items = snapshot.data!;
+              return ListView.builder(
+                itemCount: items.length + 1,
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == 0) return SpendingChart(items: items);
+                  final item = items[index - 1];
+                  return Container(
+                    margin: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                        width: 2.0,
+                        color: getCategoryColor(item.category),
+                      ),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black26,
+                          offset: Offset(0, 2),
+                          blurRadius: 6.0,
+                        ),
+                      ],
+                    ),
+                    child: ListTile(
+                      title: Text(item.name),
+                      subtitle: Text(
+                        '${item.category}',
+                      ),
+                      trailing: Text(
+                        '-\$${item.price.toStringAsFixed(2)},',
+                      ),
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              final failure = snapshot.error as Failure;
+              return Center(child: Text(failure.message));
+            }
+            // Show a loadinmg spinner
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
+      ),
+    );
+  }
+}
+
+Color getCategoryColor(String category) {
+  switch (category) {
+    case 'Entertainment':
+      return Colors.red[400]!;
+    case 'Food':
+      return Colors.green[400]!;
+    case 'Personal':
+      return Colors.blue[400]!;
+    case 'Transportation':
+      return Colors.orange[400]!;
+    default:
+      return Colors.purple[400]!;
+  }
+}
